476331f165447806db5fabd22d336012a27e0447
Manoj Mathew <mmathew4788@gmail.com>
2023-01-04 15:28:34 +0000

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState } from "react";
 
 function App() {
+
+  const InitialList = []; //Initial list
+  
+  // handle click of button
+  const [List, setList] = useState(InitialList);
+  function handleAdd() {
+    const newList = List.concat({name, id: Math.random().toString()});
+    setList(newList);
+    setName('');//clears the input field
+  }
+
+  //handle change of input field
+  const [name, setName] = useState("");
+  function handleChange(event) {
+    setName(event.target.value);
+  }
+
+  // Generating JSX code for Displaying each item
+  const ListItem = List.map((Item, Index) => {
+    return <p key={Item.id} className="ListItem">{Item.name}</p>;
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex h-screen justify-center">
+      <div className="card-container">
+        <div className="input-container">
+          <input type="text" value={name}  onChange={handleChange} className="mx-3 p-2"/>
+          <button type="button" onClick={handleAdd} className="button"> Add Item </button>
+        </div>
+        <div className="space-y-3 m-3">
+          <div className="label">List of Items</div>
+          {ListItem}
+        </div>
+      </div>
     </div>
   );
 }
